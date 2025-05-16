9b998b045c72dbfbb3c195447bc10afb6cb74cde
Godwin Tarboh <108156551+theprestigious@users.noreply.github.com>
2025-05-16 13:57:20 -0300

    Add files via upload

---
+++ CS1063-Assignment2.py
@@ -0,0 +1,63 @@
+#Multiplication and Addition_______________
+Q1 = 12 * (15 - 4) + 8**2 
+print(Q1) 
+
+#Subtraction and Division_______________ 
+Q2 = (36 + 24) * (7 - 3)/4 - 4
+print(Q2)
+
+#Exponentiation and Multiplication_______________
+
+Q3 = 5**2 + 6 * 3/8 - 2
+print(Q3)
+
+#Division and Exponentiation_______________ 
+Q4 = (9 + 5)/2 * (5**2 - 3**2) * (5 - 3)**2
+print(Q4)
+
+#Subtraction and Division_______________ 
+Q5 = (7 * (5 + 3))/2 - 102 - 1/1 - 2
+print(Q5)
+
+#Using math module_______________ 
+#Q2 
+import math 
+pie = math.pi
+r = 7
+A = math.pi * r**2
+print(A)
+
+#Quadratic formula_______________ 
+#Q2_2
+import math
+a = 1 
+b = -3 
+c = 2
+x = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+print(x)
+
+#Simple Interest_______________
+#2_3
+p = 1000 
+r = 0.05 
+t = 3
+SI = (p * r * t) 
+print(SI)
+
+#Volume of a cylinder_______________
+#Q2_4
+import math
+pie = math.pi
+r = 5
+h = 10
+V = math.pi * r**2 * h
+print(V)
+
+#Ideal Gas Law_______________ 
+#Q2_5
+P = 101325 
+V = 0.0224 
+n = 1 
+R = 8.314 
+T = (P * V) / (n * R)
+print(T)
